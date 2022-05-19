70782273aa2cf1475a669093bff34adabe1bd8ad
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-05-18 20:36:55 -0700

    Hello, Mr. Radio

--- assets/JS/quiz.js
+++ assets/JS/quiz.js
@@ -1,6 +1,7 @@
 var savedName = document.getElementById('saved-name');
 var grade = document.getElementById('grades');
 var comment = document.getElementById('msg');
+var correct = document.getElementById('radio4');
 
 function saveHighScore() {
     var scoreBoard = {
@@ -14,10 +15,17 @@ function saveHighScore() {
 function timer(){
     var sec = 75;
     var timer = setInterval(function(){
-        document.getElementById('TimerDisplay').innerHTML='75'+sec;
-        sec--
+        document.getElementById('TimerDisplay').innerHTML= 'Timer: ' + sec;
+        // sec--
+        console.log(sec--)
         if (sec < 0) {
             clearInterval(timer);
         }
     }, 1000);
 }
+
+timer();
+
+console.log('correct', correct.value);
+
+
