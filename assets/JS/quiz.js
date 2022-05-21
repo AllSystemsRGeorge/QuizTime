7bbea058d34769ec335fac1611c1c70634f7e160
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-05-20 19:20:14 -0700

    trying to add a function code to JS

--- assets/JS/quiz.js
+++ assets/JS/quiz.js
@@ -1,74 +1,72 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const scoreText = document.querySelector('#score');
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let availableQuestion = [];
-
 var questions = [
     {
-    question: 'Q1: When was the Nintendo Entertainment System launched?',
-    choice1: '1. 1980',
-    choice2: '2. 1979',
-    choice3: '3. 1985',
-    choice4: '4. 1991',
+    question: 'When was the Nintendo Entertainment System launched?',
+    options: ['1. 1980', '2. 1979', '3. 1985','4. 1991'],
     answer: 2,
     },
 
     {
-    question: 'Q2: Which main character fights against the Shinra company?',
-    choice1: '1. Mario',
-    choice2: '2. Master Chief',
-    choice3: '3. Cloud Strife',
-    choice4: '4. Crash Bandicoot',
+    question: 'Which main character fights against the Shinra company?',
+    options: ['1. Mario', '2. Master Chief', '3. Cloud Strife','4. Crash Bandicoot'],
+    answer: 2,
+    },
+
+    {
+    question: 'Which came first??',
+    options: ['1. Call of Duty: Advanced Warfare', '2. Call of Duty: Warzone', '3. Call of Duty: Black Ops', '4. Call of Duty 4: Modern Warfare'],
     answer: 3,
     },
 
     {
-    question: 'Q3: Which came first??',
-    choice1: '1. Call of Duty: Advanced Warfare',
-    choice2: '2. Call of Duty: Warzone',
-    choice3: '3. Call of Duty: Black Ops',
-    choice4: '4. Call of Duty 4: Modern Warfare ',
-    answer: 4,
+    question: 'If you see a glowing object on a video game boss, that is its...',
+    options: ['1. Strength', '2. Stamina', '3. Weakness', '4. Tickle, tickle, tickle!'],
+    answer: 2,
     },
 
     {
-    question: 'Q4: If you see a glowing object on a video game boss, that is its...',
-    choice1: '1. Strength',
-    choice2: '2. Stamina',
-    choice3: '3. Weakness',
-    choice4: '4. Tickle, tickle, tickle!',
+    question: 'Would you kindly...?',
+    options: ['1. Stomp on a goomba', '2. Shoot that enemy', '3. ...kindly do what?', '4. A man chooses, a slave obeys!'],
     answer: 3,
-    },
-
-    {
-    question: 'Q5: Would you kindly...?',
-    choice1: '1. Stomp on a goomba',
-    choice2: '2. Shoot that enemy',
-    choice3: '3. ...kindly do what?',
-    choice4: '4. A man chooses, a slave obeys!',
-    answer: 4,
     },
 ];
 
 var rightAnswer = 0
 
+
 function generateQuiz(){
-    var numberOfQuiz = questions.length
-    var n = Math.floor(Math.random() * numberOfQuiz)
+    var NumberOfQuiz = questions.length
+    var n = Math.floor(Math.random() * NumberOfQuiz)
     var question = questions[n]
     var questionTitle = document.querySelector('#questionTitle')
     var options = document.querySelector('#options')
     questionTitle.innerHTML = question.question
     options.innerHTML = ''
     for (opt in question.options) {
-        options.innerHTML = options.innerHTML + '<input type = "radio" name = "answers" value = "' + opt + '"/>' + questipn.options
+        options.innerHTML = options.innerHTML + '<input type = "radio" name = "answers" value = "' + opt + '"/>' + question.options
         [opt] + '<br />'
     }
-    rightAnser = question.answer
+    rightAnswer = question.answer
 };
+
+function submit() {
+    var selected = document.querySelector('input[name="answers"]:checked')
+    if (selected) {
+        var answer = document.querySelector('input[name="answers"]:checked').value
+        if (answer == rightAnswer)
+            alert('Correct!')
+        else
+            alert('Wrong!')
+    }
+        else
+            alert('Time keeps on slipping!')
+}
+
+function endGame() {
+    if questions {
+
+        window.location.href = 'end.html'
+    }
+}
 
 
 function timer(){
@@ -79,7 +77,12 @@ function timer(){
         console.log(sec--)
         if (sec < 0) {
             clearInterval(timer);
+            window.location.href = 'end.html';
         }
+        else (sec > 0)
+            display.innerHTML= 'Timer:' + sec;
+            window.location.href = 'end.html';
+            
     }, 1000);
 }
 
